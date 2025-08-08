78d2b18d458b7811dbe538575644584f803a3d18
Alexis <alexisgh74@gmail.com>
2025-08-08 09:51:28 -0600

    Se crea sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://developers-code.vercel.app',
+  generateRobotsTxt: true, // También regenera robots.txt
+  changefreq: 'monthly',
+  priority: 0.8,
+  sitemapSize: 5000,
+};
